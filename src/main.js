bc77d4b0d9510acea71282fdc9933fab92abe849
Yafet Trakan T <yafet.trakan@gmai.com>
2019-11-10 12:56:12 +0700

    first commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 import Vue from 'vue'
-import App from './App.vue'
+import App from './App'
+import Axios from 'axios'
+import router from './router'
+import vuetify from './plugins/vuetify';
+
+Vue.prototype.$http = Axios;
+Vue.prototype.$apiUrl = 'http://127.0.0.1:81/modul11/index.php';
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  router,
+  vuetify,
+  components: { App }
 }).$mount('#app')
